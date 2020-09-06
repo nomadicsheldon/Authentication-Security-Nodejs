094dbf5d45ffb46ff79a8b4f1cd0494299949db0
Himanshu Rajput <himanshurajput.career@gmail.com>
2020-09-06 19:12:35 +0530

    Level 3 complete- md5 Hashing

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
 const mongoose = require('mongoose');
-const encrypt = require('mongoose-encryption');
+const md5 = require('md5');
 
 // ############################## setting up frameworks ##############################
 const app = express();
@@ -29,17 +29,10 @@ mongoose.connect(url + "/" + dbName, {
   useNewUrlParser: true
 });
 
-// Level 2 Authentication ==>
 const userSchema = new mongoose.Schema({
   email: String,
   password: String
 });
-
-userSchema.plugin(encrypt, {
-  secret: process.env.SECRET,
-  encryptedFields: ['password']
-});
-// <==
 
 const User = mongoose.model('User', userSchema);
 
@@ -55,7 +48,9 @@ app.get('/login', function(req, res) {
 
 app.post('/login', function(req, res) {
   const userName = req.body.username;
-  const password = req.body.password;
+  // Level 3 ==>
+  const password = md5(req.body.password);
+  // <==
   User.findOne({
     email: userName
   }, function(err, foundUser) {
@@ -82,7 +77,9 @@ app.get('/register', function(req, res) {
 app.post('/register', function(req, res) {
   const newUser = new User({
     email: req.body.username,
-    password: req.body.password
+    // Level 3 ==>
+    password: md5(req.body.password)
+    // <==
   });
   newUser.save(function(err) {
     if (!err) {
